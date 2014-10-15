931067ef2220e5312c45326aa47120b3aa6469c4
Alex Heritier <alex.heritier@gmail.com>
2014-10-14 23:13:19 -0700

    Changed the way the skills sublist works + changed some CSS  values

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -7,43 +7,7 @@ requirejs.config({
 });
 
 require(["jquery"], function($) {
-    var masterList = {
-        "CSS": [
-            "SASS"
-        ],
-        "Javascript": [
-            "Angular.js",
-            "jQuery",
-            "Require.js"
-        ],
-        "PHP": [
-            "CodeIgniter",
-            "Laravel"
-        ]
-    };
-
-    function addHiddenSubMenu(elements, data) {
-        elements.each(function() {
-            var that = $(this),
-                text = that.find("p").text();
-
-            if (data[text] !== undefined) {
-                var subList = $("<ul class='skills-sublist'>").hide();
-                for (var i = 0; i < data[text].length; i++) {
-                    var subListLi = $("<li>"),
-                        subListP = $("<p>");
-                    subListP.text(data[text][i]);
-                    subListLi.append(subListP);
-                    subList.append(subListLi);
-                }
-                that.append(subList);
-            }
-        });
-    }
-
-    var elements = $(".skills-list li");
-    addHiddenSubMenu(elements, masterList);
-    elements.click(function() {
+    $(".skills-list li").click(function() {
         $(this).find("ul").toggle({duration: 200, easing: 'linear'});
     });
 });
